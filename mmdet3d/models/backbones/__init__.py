180a5b9d2582f1cf7fcbbcc97a4ea3912707dbc5
LongaBonga <ivan.dobryaev@gmail.com>
2021-08-20 13:29:57 +0300

    add MN_v3

--- mmdet3d/models/backbones/__init__.py
+++ mmdet3d/models/backbones/__init__.py
@@ -1,6 +1,7 @@
 from mmdet.models.backbones import SSDVGG, HRNet, ResNet, ResNetV1d, ResNeXt
 from .multi_backbone import MultiBackbone
 from .nostem_regnet import NoStemRegNet
+from .imgclsmob import *
 from .pointnet2_sa_msg import PointNet2SAMSG
 from .pointnet2_sa_ssg import PointNet2SASSG
 from .second import SECOND
